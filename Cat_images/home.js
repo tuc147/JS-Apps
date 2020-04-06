6cd4c1c241f7782a21ae3892a99732f2436317fe
tuc147 <samft99@gmail.com>
2020-04-05 20:04:15 -0400

    Adjust Styles for padding and flex

--- Cat_images/home.js
+++ Cat_images/home.js
@@ -1,5 +1,5 @@
 function generateCat(){
-    document.getElementById("cats").innerHTML = "<img src = \"https://www.shropshirestar.com/resizer/Gz0w8rBS97PJj3ksgPQHrYbDQew=/1000x0/filters:quality(100)/arc-anglerfish-arc2-prod-shropshirestar-mna.s3.amazonaws.com/public/HIQBCXT7UVGRRBNNL63AFZ6B2M.jpg\">";
+    document.getElementById("cats").innerHTML += "<img src =\"https://www.catgifpage.com/gifs/318.gif\"/>"
 }
 function resetPage(){
     document.getElementById("cats").innerHTML =null;
